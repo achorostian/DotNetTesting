750e7cb12d1e70b532975a19258e5bee9eeb4ae6
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-05-23 17:56:36 +0200

    Add Xamarin.Forms project

---
+++ Students/2018_Kulewski_Krzysztof_MVVM/CConv/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using CConv.Services.Conversion;
+using CConv.Services.CurrencyProviders;
+using CConv.ViewModels;
+using CConv.Views;
+using Xamarin.Forms;
+
+namespace CConv
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            ICurrencyConversionService conversionService = new CurrencyConversionService();
+            IList<ICurrencyProvider> providers = new List<ICurrencyProvider>
+            {
+                new FakeCurrencyProvider(),
+                new NbpCurrencyProvider(NbpTable.A),
+                new NbpCurrencyProvider(NbpTable.B)
+            };
+
+            var mainPage = new TabbedPage();
+            mainPage.Children.Add(new ConvertPage(new ConvertViewModel(conversionService, providers)));
+            mainPage.Children.Add(new AboutPage(new AboutViewModel("kkulewski.pl", "github.com/kkulewski")));
+            MainPage = mainPage;
+        }
+    }
+}
---
+++ Students/2018_Kulewski_Krzysztof_MVVM/CConv/ViewModels/AboutViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CConv.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public string HomeUrl { get; set; }
+        public string GitHubUrl { get; set; }
+
+        public AboutViewModel(string homeUrl, string githubUrl)
+        {
+            HomeUrl = homeUrl;
+            GitHubUrl = githubUrl;
+        }
+    }
+}
