8a28a6a0499032eb1fe6500361097881755cf32d
dmn <dborowiecki7@gmail.com>
2019-03-15 13:14:29 +0100

    Dodano zadanie 1

---
+++ Students/2019_Borowiecki_Damian/Zadanie1/GeometryLib/GeometryLib/Class1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryLib
+{
+    public class Point2D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+
+        public static Point2D[] ReadPointsFromFile(String filename)
+        {
+            List<Point2D> points = new List<Point2D>();
+            String[] lines = File.ReadAllLines("C:\\rearrange.txt");
+            foreach(String line in lines)
+            {
+                String[] numbers = line.Split(' ');
+                double x = Convert.ToDouble(numbers[0]);
+                double y = Convert.ToDouble(numbers[1]);
+                Point2D next = new Point2D(x, y);
+                points.Add(next);
+            }
+            return points.ToArray();
+        }
+        public Point2D(double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public String Print()
+        {
+            return String.Format("({0}, {1})", X, Y);
+        }
+        public void Scale(double multipler)
+        {
+            this.X = X * multipler;
+            this.Y = Y * multipler;
+        }
+        public void Transpose(double X = 0, double Y = 0)
+        {
+            this.X += X;
+            this.Y += Y;
+        }
+        public bool Compare(Point2D B)
+        {
+            if (X == B.X && Y == B.Y) return true;
+            return false;
+        }
+        public void MoveByVector(IVector v, double partOfVector = 1)
+        {
+            Transpose(partOfVector * v.GetVector()[0], partOfVector * v.GetVector()[1]);
+        }
+    }
+
+    public interface IVector
+    {
+        double[] GetVector();
+        double GetLength();
+        void MultiplyByNumber(double multipler);
+
+    }
+    public class Vector2D : IVector
+    {
+        private double[] vector = new double[2];
+
+        public Vector2D(Point2D start, Point2D end)
+        {
+            vector[0] = end.X - start.X;
+            vector[1] = end.Y - start.Y;
+        }
+        public Vector2D(double a, double b)
+        {
+            vector[0] = a;
+            vector[1] = b;
+        }
+        public double[] GetVector()
+        {
+            return vector;
+        }
+        public double GetX()
+        {
+            return vector[0];
+        }
+        public double GetY()
+        {
+            return vector[1];
+        }
+
+        public Vector2D Add(Vector2D v)
+        {
+            double a = v.GetX() - GetX();
+            double b = v.GetY() - GetY();
+            return new Vector2D(a, b);
+        }
+
+        public double GetLength()
+        {
+            return Math.Sqrt(Math.Pow(GetX(), 2) + Math.Pow(GetY(), 2));
+        }
+
+        public void MultiplyByNumber(double number)
+        {
+            vector[0] *= number;
+            vector[1] *= number;
+        }
+    }
+    interface IShape2D
+    {
+        Point2D[] GetPoints();
+        double Area();
+        double Circut();
+        void Scale(double multiplier);
+        void MoveAlongVector(IVector v, double partOfVector);
+
+    }
+    public class Polygon : IShape2D
+    {
+        Point2D[] points = new Point2D[4];
+        //Points have to be in connection order, won't work in other case
+        public Polygon(Point2D[] coordinates)
+        {
+            this.points = coordinates;
+        }
+        public double Area()
+        {
+            double area = 0;
+            int numberOfPoints = points.Length;
+            for (int i = 0; i < numberOfPoints - 1; i++)
+            {
+                area += points[i].X * points[i + 1].Y - points[i].Y * points[i + 1].X;
+            }
+            area += points[numberOfPoints - 1].X * points[0].Y - points[numberOfPoints - 1].Y * points[0].X;
+            area = Math.Abs(area / 2);
+            return area;
+        }
+
+        public double Circut()
+        {
+            double circut = 0;
+            Vector2D v = new Vector2D(points[points.Length - 1], points[0]);
+            circut += v.GetLength();
+            Console.WriteLine(v.GetLength());
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                v = new Vector2D(points[i], points[i + 1]);
+                Console.WriteLine(v.GetLength());
+                circut += v.GetLength();
+            }
+
+            return circut;
+        }
+
+        public Point2D[] GetPoints()
+        {
+            return points;
+        }
+
+        //Scaling in relative to point
+        public void ScaleRelative(double multiplier, Point2D pointZero)
+        {
+            foreach(Point2D p in points) {
+                if (p.Equals(pointZero)) continue;
+                IVector transposeVector = new Vector2D(pointZero, p);
+                transposeVector.MultiplyByNumber(multiplier-1);
+                p.MoveByVector(transposeVector);
+            }
+        }
+        public void Scale(double multiplier)
+        {
+            ScaleRelative(multiplier, points[0]);
+        }
+        public void MoveAlongVector(IVector v, double partOfVector = 1)
+        {
+            if (v.GetLength() != 2) throw new ArgumentException("Vector should have 2 numbers");
+            foreach (Point2D p in points)
+            {
+                p.MoveByVector(v, partOfVector);
+            }
+        }
+    }
+}
---
+++ Students/2019_Borowiecki_Damian/Zadanie1/GeometryLib/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using GeometryLib;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class PointUnitTest
+    {
+        Point2D testPoint1;
+        Point2D testPoint2;
+        Point2D testPoint3;
+        [TestInitialize]
+        public void Setup()
+        {
+            testPoint1 = new Point2D(11.0, 20.0);
+            testPoint2 = new Point2D(11.0, 20.0);
+            testPoint3 = new Point2D(10.0, 20.0);
+        }
+        [TestMethod]
+        public void TestPointComparatorPositive()
+        {
+            // Arrange:
+
+            // Act:
+            bool result = testPoint1.Compare(testPoint2);
+
+            // Assert:
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void TestPointComparatorNegtive()
+        {
+            // Arrange:
+            Point2D testPoint2 = new Point2D(13, 20);
+
+            // Act:
+            bool result = testPoint1.Compare(testPoint2);
+
+            // Assert:
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void TestMovingByVector()
+        {
+            // Arrange:
+            double[] expected = { 15.0, 23.0 };
+
+            IVector vector =
+             new GeometryLib.Fakes.StubIVector()
+             {
+                 GetVector = () => { return new double[] { 4, 3 }; }
+             };
+
+            // Act:
+            testPoint1.MoveByVector(vector, 1);
+            // Assert:
+            CollectionAssert.AreEqual(expected,
+                (new[] { testPoint1.X, testPoint1.Y }));
+        }
+        [TestMethod]
+        public void TestPointTranspose()
+        {
+            // Arrange:
+            double[] expected = { 13.0, 21.0 };
+
+            // Act:
+            testPoint1.Transpose(2, 1);
+            // Assert:
+            CollectionAssert.AreEqual(expected,
+                (new[] { testPoint1.X, testPoint1.Y }));
+        }
+        [TestMethod]
+        public void TestPointScale()
+        {
+            // Arrange:
+            double[] expected = { 5.5, 10.0 };
+
+            // Act:
+            testPoint1.Scale(0.5);
+            // Assert:
+            CollectionAssert.AreEqual(expected,
+                (new[] { testPoint1.X, testPoint1.Y }));
+        }
+        [TestMethod]
+        public void TestReadPointsFromFile()
+        {
+            // Arrange:
+            double[] expected = { 5.5, 10.0 };
+
+            using (ShimsContext.Create())
+            {
+                System.IO.Fakes.ShimFile.ReadAllLinesString =
+                    s => new string[] { "10 10", "12 32", "1 4" };
+
+            // Act
+            Point2D[] output = Point2D.ReadPointsFromFile("test.txt");
+            //
+            Assert.AreEqual(3, output.Length);
+            }
+        }
+        [TestMethod]
+        public void TestPrintPointAsAtring()
+        {
+            //Arrange:
+            String output;
+            //Act:
+            output = testPoint1.Print();
+            //Assert:
+            StringAssert.Equals(output, "11 20");
+        }
+    }
+
+    [TestClass]
+    public class VectorUnitTest
+    {
+        Vector2D vector1;
+        Vector2D vector2;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            vector1 = new Vector2D(-3, 4);
+            vector2 = new Vector2D(2, 3);
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            //Arrange
+            Vector2D outVector;
+            double[] expcted = { -1, 7 };
+        
+            //Act
+            outVector = vector1.Add(vector2);
+            //Assert
+            CollectionAssert.Equals(expcted, outVector);
+        }
+        [TestMethod]
+        public void TestGetLength()
+        {
+            //Arrange
+            double expectedLen = 5;
+            double calculatedLen;
+            //Act
+            calculatedLen = vector1.GetLength();
+            //Assert
+            Assert.AreEqual(expectedLen, calculatedLen);
+        }
+        [TestMethod]
+        public void MultiplyByNumber()
+        {
+            //Arrange
+            double multipler = 0.28;
+            double[] expected = { 0.28 * -3, 0.28 * 4 };
+            double[] actual;
+            //Act
+            vector1.MultiplyByNumber(multipler);
+            actual = new double[] { vector1.GetX(), vector1.GetY()};
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestGetVector()
+        {
+            //Arrange
+            double[] expected = new double[] { 2, 3 };
+            double[] actual;
+            //Act
+            actual = vector2.GetVector();
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestPointConstructor()
+        {
+            //Arrange
+            Point2D p1 = new Point2D(1, 2);
+            Point2D p2 = new Point2D(2, 3);
+            double[] expected = new double[] { 1, 1 };
+            double[] actual;
+            //Act
+            Vector2D createdVector = new Vector2D(p1, p2);
+            actual = createdVector.GetVector();
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+
+
+    [TestClass]
+    public class PolygonUnitTest
+    {
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+        Polygon testPolygon;
+        [TestInitialize]
+        public void Setup()
+        {
+            Point2D[] points = {
+                new Point2D(1, 1),
+                new Point2D(1, 2),
+                new Point2D(2, 2),
+                new Point2D(2, 1)
+            };
+            testPolygon = new Polygon(points);
+            //TODO: data driven test with points
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "test_data.csv", 
+            "test_data#csv",
+            DataAccessMethod.Sequential),
+            DeploymentItem("test_data.csv")]
+        [TestMethod]
+        public void TestCountArea_FromDataSource()
+        {
+            // Access the data  
+            String[] rawPoints = Convert.ToString(TestContext.DataRow["PointPosition"]).Split(';');
+            List<Point2D> points = new List<Point2D>();
+            foreach(String point in rawPoints)
+            {
+                String[] position = point.Split('|');
+                int x = Convert.ToInt32(position[0]);
+                int y = Convert.ToInt32(position[1]);
+                points.Add(new Point2D(x, y));
+            }
+            double expectedArea = Convert.ToDouble(TestContext.DataRow["ExpectedArea"]);
+            Polygon p = new Polygon(points.ToArray());
+            double actual = p.Area();
+            Assert.AreEqual(expectedArea, actual, 0.001);
+
+        }
+        [TestMethod]
+        public void TestCircutCounting()
+        {
+            //Arrange
+            double expected = 4;
+            //Act
+            double actual = testPolygon.Circut();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        private bool PointListComparator(Point2D[] a, Point2D[] b)
+        {
+            if (a.Length != b.Length) return false;
+            for(int i = 0; i < a.Length; i++)
+            {
+                Point2D p1 = a[0];
+                Point2D p2 = b[0];
+                if (p1.X != p2.X || p1.Y != p2.Y) return false;
+            }
+            return true;
+        }
+        [TestMethod]
+        public void TestMoveAlongVector()
+        {
+            //Arrange
+            Point2D[] actual;
+            Point2D[] expected = {
+                new Point2D(2,2),
+                new Point2D(3,2),
+                new Point2D(3,3),
+                new Point2D(2,3)
+            };
+            IVector vector =
+            new GeometryLib.Fakes.StubIVector()
+            {
+                GetVector = () => { return new double[] { 1, 1 }; },
+                GetLength = () => { return 2; }
+            };
+            //Act
+            testPolygon.MoveAlongVector(vector);
+            actual = testPolygon.GetPoints();
+            //Assert
+            Console.WriteLine("{0}--{1}", actual[0].Print(), expected[0].Print());
+            Assert.IsTrue(PointListComparator(expected, actual));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestErrorMovingAlongVector()
+        {
+            //Arrange
+            IVector vector =
+            new GeometryLib.Fakes.StubIVector()
+            {
+                GetLength = () => { return 3; }
+            };
+            //Act
+            testPolygon.MoveAlongVector(vector);
+        }
+
+        [TestMethod]
+        public void TestScalingPolygon()
+        {
+            //Arrange
+            double beforeScale = testPolygon.Circut();
+            double scale = 0.5;
+            double expected = beforeScale * scale;
+            //Act
+            testPolygon.Scale(scale);
+            //Arrange
+            Assert.AreEqual(expected, testPolygon.Circut());
+        }
+
+    }
+}
